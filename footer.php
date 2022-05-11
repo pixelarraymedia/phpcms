34a74813802ba7fcaea158bf46ed310e4dc4e7a5
Darion <pixelarraymedia@gmail.com>
2022-05-11 05:59:07 -0700

    moved querys from chart-data

--- footer.php
+++ footer.php
@@ -1,17 +1,13 @@
 <div class="fbg">
     <div class="fbg_resize">
       <div class="col c1">
-        <h2><span>Property Image</span></h2>
-        <a href="#"><img src="images/pix1.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="images/pix2.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="images/pix3.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="images/pix4.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="images/pix5.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="images/pix6.jpg" width="58" height="58" alt="" /></a> </div>
+       
       <div class="col c2">
         <h2><span>Lorem Ipsum</span></h2>
-        <p>Lorem ipsum dolor<br />
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. Cras id urna. <a href="#">Morbi tincidunt, orci ac convallis aliquam</a>, lectus turpis varius lorem, eu posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum odio, ac blandit ante orci ut diam.</p>
+      
       </div>
       <div class="col c3">
-        <h2><span>About</span></h2>
-        <img src="images/white.jpg" width="56" height="56" alt="" />
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu posuere nunc justo tempus leo. llorem, eu posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum. <a href="#">Learn more...</a></p>
+        
       </div>
       <div class="clr"></div>
     </div>
